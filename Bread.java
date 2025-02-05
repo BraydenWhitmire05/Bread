a7dfd6cf0ecce2908c42613e247e0e2bd81fd85e
BraydenWhitmire05 <bwhitmire958@gmail.com>
2025-02-05 14:22:58 -0500

    Update Bread.java

--- Bread.java
+++ Bread.java
@@ -1,46 +1,153 @@
-/*
- * 
- * @class: Bread.java
- * @author:
- * @version
- * course:
- * written on:
- * description:
- */
-
 public class Bread {
-	//flour - boolean, sugar - double, eggs - int
-	private boolean glutenFree;
-	private Ingredients ingredients = new Ingredients();
-	private Recipe recipeBread = new Recipe();
-	
-	public Bread() {
-		
+
+	    private String breadName;
+	    private boolean state;
+	    private String shape;
+	    private double flour;
+	    private int water;
+	    private double salt;
+	    private double sugar;
+	    private double bakingPowder;
+	    private double yeast;
+	    private String recipe;
+
+
+	    public Bread(){
+	        breadName = "Plain Bread";
+	        shape = "loaf";
+	        state = false;
+	        flour = 1;
+	        water = 1;
+	        salt = 1;
+	        sugar = 1;
+	        bakingPowder = 1;
+	        yeast = 1;
+	        recipe = "Plain bread's recipe goes here.";
+	    }
+
+	    public Bread(String breadName, String shape,boolean state, double flour, int water, double salt, double sugar, double bakingPowder, double yeast, String recipe) {
+	        this.breadName = breadName;
+	        this.shape = shape;
+	        this.state = state;
+	        this.flour = flour;
+	        this.water = water;
+	        this.salt = salt;
+	        this.sugar = sugar;
+	        this.bakingPowder = bakingPowder;
+	        this.yeast = yeast;
+	        this.recipe = recipe;
+	    }
+
+	    public String getBreadName() {
+	        return breadName;
+	    }
+
+	    public void setBreadName(String breadName) {
+	        this.breadName = breadName;
+	    }
+
+	    public boolean isState() {
+	        return state;
+	    }
+
+	    public void setState(boolean state) {
+	        this.state = state;
+	    }
+
+	    public double getFlour() {
+	        return flour;
+	    }
+
+	    public void setFlour(double flour) {
+	        this.flour = flour;
+	    }
+
+	    public int getWater() {
+	        return water;
+	    }
+
+	    public void setWater(int water) {
+	        this.water = water;
+	    }
+
+	    public double getSalt() {
+	        return salt;
+	    }
+
+	    public void setSalt(double salt) {
+	        this.salt = salt;
+	    }
+
+	    public double getSugar() {
+	        return sugar;
+	    }
+
+	    public void setSugar(double sugar) {
+	        this.sugar = sugar;
+	    }
+
+	    public double getBakingPowder() {
+	        return bakingPowder;
+	    }
+
+	    public void setBakingPowder(double bakingPowder) {
+	        this.bakingPowder = bakingPowder;
+	    }
+
+	    public double getYeast() {
+	        return yeast;
+	    }
+
+	    public void setYeast(double yeast) {
+	        this.yeast = yeast;
+	    }
+
+	    public String getRecipe() {
+	        return recipe;
+	    }
+
+	    public void setRecipe(String recipe) {
+	        this.recipe = recipe;
+	    }
+	    public String getShape() {
+			return shape;
+		}
+
+		public void setShape(String shape) {
+			this.shape = shape;
+		}
+
+	    //double flour, int water, double salt, double sugar,
+	// double bakingPowder, double yeast) {
+	    public String getIngredients(){
+	        return String.format("%.2f cups of flour\n%d cups of water\n%.2f table spoons of salt" +
+	                "\n%.2f cups of sugar\n%.2f tsp of Baking Powder\n%.2f tsps of yeast",flour,water,salt, sugar, bakingPowder, yeast);
+	    }
+
+	    public boolean isBaked(){
+	        if(state){
+	            System.out.println(breadName + " is baked");
+	            return true;
+	        }
+	        else {
+	            System.out.println(breadName + " is not baked");
+	            return false;
+	        }
+	    }
+
+	    public boolean isBaked(boolean state){
+
+	        this.state = state;
+	        if(this.state){
+	            System.out.println(breadName + " is baked");
+	            return true;
+	        }
+	        else {
+	            System.out.println(breadName + " is not baked");
+	            return false;
+	        }
+	    }
+	    public String toString(){
+	        return String.format("%s\n%s\n%s\n%s\n",breadName,shape,getIngredients(),recipe);
+	    }
 	}
-	public Bread(boolean glutenFree, Ingredients ingredients) {
-		this.glutenFree = glutenFree;
-		this.ingredients = ingredients;
-	}
-	public boolean isGlutenFree() {
-		return glutenFree;
-	}
-	public void setGlutenFree(boolean glutenFree) {
-		this.glutenFree = glutenFree;
-	}
-	public Ingredients getIngredients() {
-		return ingredients;
-	}
-	public void setIngredients(Ingredients ingredients) {
-		this.ingredients = ingredients;
-	}
-	public Recipe getRecipeBread() {
-		return recipeBread;
-	}
-	public void setRecipeBread(Recipe recipeBread) {
-		this.recipeBread = recipeBread;
-	}
-	public String toString() {
-		return "Basic Bread: \n " + ingredients + "\n";
-	}
-	
-}
