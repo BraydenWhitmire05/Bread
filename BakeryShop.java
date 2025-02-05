3d0f3f6814ab0bebb6331ff04de97b1d0a8c3e6b
BraydenWhitmire05 <bwhitmire958@gmail.com>
2025-02-05 15:40:16 -0500

    Update BakeryShop.java

--- BakeryShop.java
+++ BakeryShop.java
@@ -1,10 +1,21 @@
-
 public class BakeryShop {
 	public static void main(String[] args) {
-		Bread wonderfulBread = new Bread();
-		System.out.println(wonderfulBread.getIngredients());
-		wonderfulBread.getRecipeBread().recipe();
-
+	     
+	      SourdoughBread_ sd = new SourdoughBread_("Sourdough Bread","Oval Shape",true,10,10,10,10,10,10,"FULL RECIPE:");
+	      System.out.println(sd);
+	      
+	      StrawberryShortcake_ sc = new StrawberryShortcake_("\nStrawberry Shortcake","Circle Shape",true,10,10,10,10,10,10,"FULL RECIPE:");
+	      System.out.println(sc);
+	      
+	      RyeBread_ rb = new RyeBread_("\nRye Bread","Square Shape",true,10,10,10,10,10,10,"FULL RECIPE:");
+	      System.out.println(rb);
+	      
+	      WhiteBread_ wb = new WhiteBread_("\nWhite Bread","Loaf Shape",true,10,10,10,10,10,10,"FULL RECIPE:");
+	      System.out.println(wb);
+	      
+	      WheatBread_ wtb = new WheatBread_("\nWheat Bread","Loaf Shape",true,10,10,10,10,10,10,"FULL RECIPE:");
+	      System.out.println(wtb);
+	      
 	}
 
 }
